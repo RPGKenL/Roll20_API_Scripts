ee7d368d3f9fe56c2fa10e224b8ed66b32be6147
Kenny Law <KennyLaw@member.fsf.org>
2015-03-08 18:45:31 -0400

    accidentially reverted the license, returning GPL

--- growit.js
+++ growit.js
@@ -2,13 +2,18 @@
  * GrowIt 0.5
  * 
  * Copyright 2015: Ken L.
- * Dual licensed under the MIT or GPL Version 3 licenses.
- * https://gist.github.com/Roll20KenL/fc24f527a77f13f4f29f
- * 
- * MIT: 
- * http://opensource.org/licenses/mit-license.php
- * GPL v3: 
+ * Licensed under the GPL Version 3 license.
  * http://www.gnu.org/licenses/gpl.html
+ * 
+ * This script is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  * 
  * Grows a token using PF measurements
  * 
